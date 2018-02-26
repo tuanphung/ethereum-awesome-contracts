b2a25ddecb3105aa68b1091d1517a68f218dc239
tuanphung <tuanphunglk@gmail.com>
2018-02-26 23:03:01 +0700

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,22 @@
+ ^0.4.20;
+
+contract Ownable {
+    address private _owner;
+
+    function Ownable() public {
+        _owner = msg.sender;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        _owner = newOwner;
+    }
+
+    function owner() public constant returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == _owner);
+        _;
+    }
+}
